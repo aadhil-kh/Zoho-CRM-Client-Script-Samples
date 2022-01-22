89b6c2902b9eaad440b650981085eb5c2048694c
Aadhil <aadhilkh2011@gmail.com>
2022-01-22 14:31:28 +0530

    onload population

---
+++ onLoadPrepopulation.js
@@ -0,0 +1,19 @@
+/**
+ * Module: Leads
+ * Page: Create record
+ * Event: onLoad
+ * Description: Prepopulate some form data based on logged-in user role and make it read-only
+ */
+
+const user_data = ZDK.Apps.CRM.Users.fetchById($Crm.user.id); // $Crm.user -> logged in user data
+
+const role_vs_lead_source = {
+    'Facebook Admin': 'Facebook',
+    'Twitter Admin': 'Twitter',
+    'Desk Admin': 'Chat'
+};
+
+const lead_source_field = ZDK.Page.getField('Lead_Source');
+
+lead_source_field.setValue(role_vs_lead_source[user_data.role.name]);
+lead_source_field.setReadOnly(true);
