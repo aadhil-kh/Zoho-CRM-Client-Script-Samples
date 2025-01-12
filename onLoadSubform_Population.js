6906f9683018bbba39783c0b184858f402cfb666
Aadhil <aadhilkh2011@gmail.com>
2025-01-12 13:44:39 +0530

    Create onLoadSubform_Population.js

---
+++ onLoadSubform_Population.js
@@ -0,0 +1,13 @@
+/**
+ * Module: Leads
+ * Page: Create record
+ * Event: onLoad
+ * Description: Prepopulate dummy subform data based on 
+ */
+
+const picklist_options = ZDK.Page.getField('Picklist_1').getPicklistOptions();
+
+// add rows to the subform
+ZDK.Page.getField('Subform_1').setValue(picklist_options.map(option => ({
+  Document_Name: option.display_value
+})));
